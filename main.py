3db8b17ed3e3a085c5afeea6d7185767132030c1
Fae <109899113+iamf-a-e@users.noreply.github.com>
2025-07-15 12:19:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ phone_id = os.environ.get("PHONE_ID")
 gen_api = os.environ.get("GEN_API")
 owner_phone = os.environ.get("OWNER_PHONE")
 GOOGLE_MAPS_API_KEY = "AlzaSyCXDMMhg7FzP|ElKmrlkv1TqtD3HgHwW50"
-AGENT_NUMBER = "+263779562095"
+AGENT_NUMBER = "+263719835124"
 AGENT_NUMBER1 = "+263776954188"
 
 # Upstash Redis setup
